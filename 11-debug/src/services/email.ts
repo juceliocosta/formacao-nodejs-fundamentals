426e76d81b27a2ca188022bb4eb1264e35ab84e4
Jucélio José <juceliojdc@gmail.com>
2025-09-19 18:21:06 -0300

    debug

---
+++ 11-debug/src/services/email.ts
@@ -0,0 +1,16 @@
+async function getBaseEmail(senderName:string):Promise<string>{
+    let base = await getHeaderText();
+    base += `\nOlá ${senderName}, gostaria de me inscrever para uma vaga.`;
+
+    base += "\nEstou deixando o meu currículo.";
+
+    return base;
+}
+
+async function getHeaderText(): Promise<string> {
+    return "EMAIL PARA VOCê";
+}
+
+export {
+    getBaseEmail
+}
